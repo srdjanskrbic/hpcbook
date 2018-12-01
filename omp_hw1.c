f923a89085f1303c279d112d90f688f466a20aff
Srdjan Skrbic <30725195+srdjanskrbic@users.noreply.github.com>
2018-12-01 16:57:48 +0100

    Update omp_hw1.c

--- omp_hw1.c
+++ omp_hw1.c
@@ -30,5 +30,5 @@ int main(int argc, char* argv[]) {
 void Hello(void) {
 	int my_rank = omp_get_thread_num();
 	int thread_count = omp_get_num_threads();
-	printf("Hello from thread %d of %d\n", my_rank, thread_count);
+	printf("Zdravo od niti %d od %d\n", my_rank, thread_count);
 }
